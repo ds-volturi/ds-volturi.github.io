a749b693b639886ceb0f31ecc6d7df7de5b20e8c
ds-volturi <36482978+ds-volturi@users.noreply.github.com>
2018-02-16 16:55:48 +0300

    Add files via upload

--- submit.php
+++ submit.php
@@ -2,7 +2,7 @@
 if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['message'])) 
 {
     $headers = 'From: Кротов Роман\r\n' .
-                'Reply-To: reget1986@mail.ru\r\n' .
+                'Reply-To: ds-volturi.github.io\r\n' .
                 'X-Mailer: PHP/' . phpversion();
          
     $theme = "Новое сообщение с сайта";             
@@ -14,7 +14,7 @@ if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['messag
     $letter .="\nТелефон: ".$_POST['phone'];
     $letter .="\nСообщение: ".$_POST['message'];
     
-    if (mail("reget1986@mail.ru", $theme, $letter, $headers)){
+    if (mail("ds-volturi.github.io", $theme, $letter, $headers)){
       header("Location: /testform/thankyou.php");
     } else {
       header("Location: /testform");
